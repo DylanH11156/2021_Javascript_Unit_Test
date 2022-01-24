18ee282f0244aa61495c7a6e944f1a80cbc14f9e
DylanH11156 <dylan.hammer@vtsd.com>
2022-01-24 10:04:37 -0500

    Java Application Notes

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,9 +1,11 @@
 /* Put code for the task below */
-function showButton(){
-	var btn = document.getElementById("btn");
+function byeBye() {
+	var btn = document.getElementById("buttonTask1");
 	btn.style.display = "block";
+}
+function functionName(){
+	var img = document.getElementById("imageTask1");
+	img.style.display = "none";
 
-	var image = document.getElementById("meme");
-	image.style.display = "none";
 	
 }
